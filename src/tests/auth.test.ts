812e920c13dd1e7ef01a3969abd661086df71809
Jordan Marchese <jordanm62000@gmail.com>
2026-09-23 13:08:19 -0400

    add tests with forced failure

---
+++ src/tests/auth.test.ts
@@ -0,0 +1,17 @@
+import { describe, expect, test } from "vitest";
+import { getAPIKey } from "./src/api/auth.js";
+
+const headers = {
+  authorization: "ApiKey test",
+};
+
+describe ("headers", () => {
+    test("headers is defined", () => {
+        expect(headers).toBeDefined();
+    });
+
+    test("getApiKey", () => {
+        const result = getAPIKey(headers);
+        expect(result).toBe("test");
+    });
+});
